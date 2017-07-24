7aa1235a8746ad5e1187f77ef9c25dc7c06160fa
hakeempolistico <hjpolistico@gmail.com>
2017-07-24 23:32:42 +0800

    Side nav fixed temporarily

--- application/views/index.php
+++ application/views/index.php
@@ -351,155 +351,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <li class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
+            <span>Enrollment</span>
             <span class="pull-right-container">
               <span class="label label-primary pull-right">4</span>
             </span>
           </a>
+
           <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('welcome/topnav'); ?>"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-            <li><a href="<?php echo site_url('welcome/boxed'); ?>"><i class="fa fa-circle-o"></i> Boxed</a></li>
-            <li><a href="<?php echo site_url('welcome/fixed'); ?>"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="<?php echo site_url('welcome/colside'); ?>"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+            <li><a href="<?php echo site_url('welcome/topnav'); ?>"><i class="fa fa-circle-o text-red"></i>Dashboard</a></li>
+            <li><a href="<?php echo site_url('welcome/boxed'); ?>"><i class="fa fa-circle-o text-red"></i>Enroll Student</a></li>
+            <li><a href="<?php echo site_url('welcome/fixed'); ?>"><i class="fa fa-circle-o text-red"></i>Manage Student</a></li>
+            <li><a href="<?php echo site_url('welcome/colside'); ?>"><i class="fa fa-circle-o text-red"></i>Manage Schedule</a></li>
           </ul>
         </li>
-        <li>
-          <a href="<?php echo site_url('welcome/widgets'); ?>">
-            <i class="fa fa-th"></i> <span>Widgets</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-green">new</small>
-            </span>
-          </a>
-        </li>
+      
         <li class="treeview">
           <a href="#">
             <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
+            <span>Enrollment</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
+
           <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('welcome/chartjs'); ?>"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="<?php echo site_url('welcome/morris'); ?>"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="<?php echo site_url('welcome/flot'); ?>"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="<?php echo site_url('welcome/inline'); ?>"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-laptop"></i>
-            <span>UI Elements</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('welcome/uigeneral'); ?>"><i class="fa fa-circle-o"></i> General</a></li>
-            <li><a href="<?php echo site_url('welcome/icons'); ?>"><i class="fa fa-circle-o"></i> Icons</a></li>
-            <li><a href="<?php echo site_url('welcome/buttons'); ?>"><i class="fa fa-circle-o"></i> Buttons</a></li>
-            <li><a href="<?php echo site_url('welcome/sliders'); ?>"><i class="fa fa-circle-o"></i> Sliders</a></li>
-            <li><a href="<?php echo site_url('welcome/timeline'); ?>"><i class="fa fa-circle-o"></i> Timeline</a></li>
-            <li><a href="<?php echo site_url('welcome/modals'); ?>"><i class="fa fa-circle-o"></i> Modals</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-edit"></i> <span>Forms</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('welcome/general'); ?>"><i class="fa fa-circle-o"></i> General Elements</a></li>
-            <li><a href="<?php echo site_url('welcome/advanced'); ?>"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-            <li><a href="<?php echo site_url('welcome/editors'); ?>"><i class="fa fa-circle-o"></i> Editors</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-table"></i> <span>Tables</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('welcome/simple'); ?>"><i class="fa fa-circle-o"></i> Simple tables</a></li>
-            <li><a href="<?php echo site_url('welcome/data'); ?>"><i class="fa fa-circle-o"></i> Data tables</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="<?php echo site_url('welcome/calendar'); ?>">
-            <i class="fa fa-calendar"></i> <span>Calendar</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-red">3</small>
-              <small class="label pull-right bg-blue">17</small>
-            </span>
-          </a>
-        </li>
-        <li>
-          <a href="<?php echo site_url('welcome/mailbox'); ?>">
-            <i class="fa fa-envelope"></i> <span>Mailbox</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-yellow">12</small>
-              <small class="label pull-right bg-green">16</small>
-              <small class="label pull-right bg-red">5</small>
-            </span>
-          </a>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-folder"></i> <span>Examples</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('welcome/invoice'); ?>"><i class="fa fa-circle-o"></i> Invoice</a></li>
-            <li><a href="<?php echo site_url('welcome/profile'); ?>"><i class="fa fa-circle-o"></i> Profile</a></li>
-            <li><a href="<?php echo site_url('welcome/login'); ?>"><i class="fa fa-circle-o"></i> Login</a></li>
-            <li><a href="<?php echo site_url('welcome/register'); ?>"><i class="fa fa-circle-o"></i> Register</a></li>
-            <li><a href="<?php echo site_url('welcome/lockscreen'); ?>"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-            <li><a href="<?php echo site_url('welcome/por'); ?>"><i class="fa fa-circle-o"></i> 404 Error</a></li>
-            <li><a href="<?php echo site_url('welcome/payb'); ?>"><i class="fa fa-circle-o"></i> 500 Error</a></li>
-            <li><a href="<?php echo site_url('welcome/blank'); ?>"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-            <li><a href="<?php echo site_url('welcome/pace'); ?>"><i class="fa fa-circle-o"></i> Pace Page</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-share"></i> <span>Multilevel</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-            <li class="treeview">
-              <a href="#"><i class="fa fa-circle-o"></i> Level One
-                <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
+            <li><a href="<?php echo site_url('welcome/chartjs'); ?>"><i class="fa fa-circle-o text-aqua"></i>Dashboard</a></li>
+            <li><a href="<?php echo site_url('welcome/morris'); ?>"><i class="fa fa-circle-o text-aqua"></i>Enroll Student
+              <span class="pull-right-container">
+                  <span class="label label-primary pull-right">4</span>
                 </span>
               </a>
-              <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
-                <li class="treeview">
-                  <a href="#"><i class="fa fa-circle-o"></i> Level Two
-                    <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                  </ul>
-                </li>
-              </ul>
             </li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
+            <li><a href="<?php echo site_url('welcome/flot'); ?>"><i class="fa fa-circle-o text-aqua"></i>Manage Student</a></li>
+            <li><a href="<?php echo site_url('welcome/inline'); ?>"><i class="fa fa-circle-o text-aqua"></i>Manage Schedule</a></li>
           </ul>
         </li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+        
         <li class="header">LABELS</li>
         <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
         <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
