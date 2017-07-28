2608f30776be4e5547356bfa2b26af5edb2ff94f
hakeempolistico <hjpolistico@gmail.com>
2017-07-28 13:50:47 +0800

    Polishing of UI

--- dist/js/enrollment/dashboard.js
+++ dist/js/enrollment/dashboard.js
@@ -67,15 +67,15 @@ $(function () {
     var PieData2        = [
       {
         value    : 1500,
-        color    : '#00c0ef',
-        highlight: '#00c0ef',
-        label    : 'Grade 11'
+        color    : 'darkred',
+        highlight: 'darkred',
+        label    : 'TVL Cookery'
       },
       {
-        value    : 700,
-        color    : '#3c8dbc',
-        highlight: '#3c8dbc',
-        label    : 'Grade 12'
+        value    : 900,
+        color    : 'darkgrey',
+        highlight: 'darkgrey',
+        label    : 'TVL Automotive Servicing'
       }
     ]
     var pieOptions2     = {
@@ -105,6 +105,55 @@ $(function () {
     //Create pie or douhnut chart
     // You can switch between pie and douhnut using the method below.
     pieChart2.Doughnut(PieData2, pieOptions2)
+
+        //-------------
+    //- PIE CHART 3 -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var pieChartCanvas3 = $('#pieChart3').get(0).getContext('2d')
+    var pieChart3       = new Chart(pieChartCanvas3)
+    var PieData3       = [
+      {
+        value    : 1500,
+        color    : 'darkred',
+        highlight: 'darkred',
+        label    : 'TVL Cookery'
+      },
+      {
+        value    : 700,
+        color    : 'darkgrey',
+        highlight: 'darkgrey',
+        label    : 'TVL Automotive Servicing'
+      }
+    ]
+    var pieOptions3     = {
+      //Boolean - Whether we should show a stroke on each segment
+      segmentShowStroke    : true,
+      //String - The colour of each segment stroke
+      segmentStrokeColor   : '#fff',
+      //Number - The width of each segment stroke
+      segmentStrokeWidth   : 2,
+      //Number - The percentage of the chart that we cut out of the middle
+      percentageInnerCutout: 50, // This is 0 for Pie charts
+      //Number - Amount of animation steps
+      animationSteps       : 100,
+      //String - Animation easing effect
+      animationEasing      : 'easeOutBounce',
+      //Boolean - Whether we animate the rotation of the Doughnut
+      animateRotate        : true,
+      //Boolean - Whether we animate scaling the Doughnut from the centre
+      animateScale         : false,
+      //Boolean - whether to make the chart responsive to window resizing
+      responsive           : true,
+      // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+      maintainAspectRatio  : true,
+      //String - A legend template
+      legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    pieChart3.Doughnut(PieData3, pieOptions3)
+
 
     //-------------
     //- BAR CHART -
@@ -170,4 +219,6 @@ $(function () {
 
     barChartOptions.datasetFill = false
     barChart.Bar(barChartData, barChartOptions)
+
+    
   })
