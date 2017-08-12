7d5051d85e018599915503df5f2061b4539d047b
hakeempolistico <hjpolistico@gmail.com>
2017-08-13 01:49:10 +0800

    Maroon theme modified

--- application/views/enrollment/view_data.php
+++ application/views/enrollment/view_data.php
@@ -21,7 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/skins/_all-skins.css">
   <!-- Morris chart -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/morris.js/morris.css">
   <!-- jvectormap -->
