a4d0a9001f509f8fa9ef58bc94ff644ad7406e93
jasveranlouise <jassysalva@gmail.com>
2017-08-13 12:25:10 -0700

    modal added

--- application/views/enrollment/manage_student.php
+++ application/views/enrollment/manage_student.php
@@ -570,8 +570,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     </table>
 
                     <hr style="margin-bottom: 10px;">
-                    <button type="button" class="btn btn-default" style="width: 15%;">Add</button>
-                    <button type="button" class="btn btn-default" style="width: 15%;">Transfer</button>
+                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default" style="width: 15%;">
+                    Add
+                    </button>
+                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default" style="width: 15%;">
+                    Transfer
+                    </button>
 
                   </div>
                   <!-- /.tab-pane -->
@@ -612,14 +616,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <!-- /.col -->
     </div>
     <!-- /.row -->
-
-
-      
-      
-
-
     </section>
     <!-- /.content -->
+    <div class="modal fade" id="modal-default">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Add Students</h4>
+              </div>
+              <div class="modal-body">
+                <p>One fine body</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Confirm</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
   </div>
   <!-- /.content-wrapper -->
   <!-- /.pag nawala to masisira footer -->
@@ -967,7 +986,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     });
     
 }); 
-
+$(function () {
+    $('#sectionTable').DataTable()
+    
+  })
 
 </script> 
 </body>
