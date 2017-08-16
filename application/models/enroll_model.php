444bc3898f0d03c5226e5efae7fb08379e70dd03
adriitakumi <nicolettekagamine@gmail.com>
2017-08-16 16:11:12 +0800

    Removed array

--- application/models/enroll_model.php
+++ application/models/enroll_model.php
@@ -5,32 +5,6 @@ class enroll_model extends CI_Model{
 public function add($data)
 {
 
-$studentInfo = array(
-	'id' => '',
-	'lrn' => $data['lrn'],
-	'fname' => $data['fname'],
-	'mname' => $data['mname'],
-	'lname' => $data['lname'],
-	'sex' => $data['sex'],
-	'contact' => $data['contact'],
-	'birthdate' => $data['birthdate'],
-	'birthplace' => $data['birthplace'],
-	'age' => $data['age'],
-	'mothertongue' => $data['mothertongue'],
-	'religion' => $data['religion'],
-	'street' => $data['street'],
-	'barangay' => $data['barangay'],
-	'city' => $data['city'],
-	'province' => $data['province'],
-	'fathername' => $data['fathername'],
-	'mothername' => $data['mothername'],
-	'fathercontact' => $data['fathercontact'],
-	'mothercontact' => $data['mothercontact'],
-	'guardian' => $data['guardian'],
-	'relationship' => $data['relationship'],
-	'guardiancontact' => $data['guardiancontact']
-	);
-	
 $this->db->insert('enroll', $data);
 
 }
