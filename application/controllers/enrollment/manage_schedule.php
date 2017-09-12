9848505c65750aaa03f21ab1d25fb8497d248ece
adriitakumi <nicolettekagamine@gmail.com>
2017-09-12 19:08:14 +0800

    Added Academics tab to some some pages in the Enrollment tab

--- application/controllers/enrollment/manage_schedule.php
+++ application/controllers/enrollment/manage_schedule.php
@@ -12,8 +12,8 @@ class manage_schedule extends CI_Controller {
 	public function index()
 	{
 		$data = $this->parse->parsed();
-		$data['classesCount'] = $this->global_model->count('classes');
-		$data['classes'] = $this->global_model->getRecords('classes');		
+		$data['classesCount'] = $this->global_model->count('sections');
+		$data['classes'] = $this->global_model->getRecords('sections');		
 		$data['teachers'] = $this->global_model->getRecords('teachers');		
 		$data['rooms'] = $this->global_model->getRecords('rooms');			
 		$data['subjects'] = $this->global_model->getRecords('subjects');		
