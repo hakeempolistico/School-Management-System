d7d5e21b2e47f74867b25b42e303a2202143fa64
jasveranlouise <jassysalva@gmail.com>
2017-10-01 17:19:39 +0800

    added signout

--- application/views/enrollment/search.php
+++ application/views/enrollment/search.php
@@ -76,7 +76,7 @@
                   <a href="#" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="<?php echo site_url('login/logout')?>" class="btn btn-default btn-flat">Sign out</a>
                 </div>
               </li>
             </ul>
