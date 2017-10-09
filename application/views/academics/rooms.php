d4c92042956cafa27c480d77410ad5faf95def2e
jasveranlouise <jassysalva@gmail.com>
2017-10-10 01:27:28 +0800

    fixed title and some code clean up

--- application/views/academics/rooms.php
+++ application/views/academics/rooms.php
@@ -1,12 +1,9 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Dashboard</title>
+  <title>Manage Rooms</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
