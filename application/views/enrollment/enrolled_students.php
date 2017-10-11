d3a9211b72a3e33140a3d3cb3c739c35ef937905
adriitakumi <nicolettekagamine@gmail.com>
2017-10-12 05:57:42 +0800

    enrolled students view

---
+++ application/views/enrollment/enrolled_students.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Search for Online Applications</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/select2/dist/css/select2.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/skins/_all-skins.min.css">
+  <!-- bootstrap datepicker -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/googlefont.css">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="<?php echo site_url('/') ?>" class="logo" style="background: rgb(97, 22, 35)">
+      {logo}
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" style="background-color: #6C1827">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">                    
+          <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="<?php echo base_url('images/alt_picture.jpg');?>" class="user-image" alt="<?php echo base_url('images/alt_picture.jpg');?>">
+              <span class="hidden-xs"><?php echo $this->session->first_name." ".$this->session->last_name ?></span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- User image -->
+              <li class="user-header" style="background-color: #6C1827">
+                <img src="<?php echo base_url('images/alt_picture.jpg');?>" class="img-circle" alt="<?php echo base_url('images/alt_picture.jpg');?>">
+
+                <p>
+                 <?php echo $this->session->first_name." ".$this->session->last_name." - ".$this->session->position ?>
+                  <small>Member since <?php echo $this->session->date_created ?></small>
+                </p>
+              </li>              
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="<?php echo site_url('profile')?>" class="btn btn-default btn-flat">Profile</a>
+                </div>
+                <div class="pull-right">
+                  <a href="<?php echo site_url('login/logout')?>" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->
+          
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="<?php echo base_url('images/alt_picture.jpg');?>" class="img-circle" alt="<?php echo base_url('images/alt_picture.jpg');?>">
+        </div>
+        <div class="pull-left info">
+          <p><?php echo $this->session->first_name." ".$this->session->last_name ?></p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li>
+          <a href="<?php echo site_url('/'); ?>">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-files-o"></i>
+            <span>Enrollment</span>
+            <span class="pull-right-container">
+              <span class="fa fa-angle-left pull-right"></span>
+            </span>
+          </a>
+
+          <ul class="treeview-menu">
+            <li>
+              <a href="<?php echo site_url('enrollment/register_student'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Register Student
+              </a>              
+            </li>
+
+            <li class="active">
+              <a href="<?php echo site_url('enrollment/enroll_student'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                  Enroll Student
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-mortar-board"></i>
+            <span>Academics</span>
+            <span class="pull-right-container">
+              <span class="fa fa-angle-left pull-right"></span>
+            </span>
+          </a>
+
+          <ul class="treeview-menu">
+            <li>
+              <a href="<?php echo site_url('academics/strands'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Strands
+              </a>
+            </li>
+            <li>
+              <a href="<?php echo site_url('academics/teachers'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Teachers
+              </a>
+            </li>
+            <li>
+              <a href="<?php echo site_url('academics/rooms'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Rooms
+              </a>
+            </li>
+            <li>
+              <a href="<?php echo site_url('academics/sections'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Sections
+              </a>
+            </li>          
+            <li>
+              <a href="<?php echo site_url('academics/subjects'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                  Subjects
+              </a>
+            </li>
+            <li>
+              <a href="<?php echo site_url('academics/assign_subjects'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Assign Subjects
+              </a>
+            </li>
+            <li>
+              <a href="<?php echo site_url('academics/schedule'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                  Schedule
+              </a>
+            </li>
+          </ul>
+          
+        </li>
+      
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-id-card"></i>
+            <span>Student Information</span>
+            <span class="pull-right-container">
+              <span class="label label-warning pull-right">soon</span>
+            </span>
+          </a>
+
+          <ul class="treeview-menu">
+            <li>
+              <a href="<?php echo site_url('enrollment/dashboard'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Student Details
+              </a>
+            </li>         
+            <li>
+              <a href="<?php echo site_url('enrollment/register_student'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Student Grades
+              </a>
+            </li>
+            <li>
+              <a href="<?php echo site_url('enrollment/enroll_student'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Student Promotion
+              </a>
+            </li>
+          </ul>          
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-bar-chart"></i>
+            <span>Reports</span>
+            <span class="pull-right-container">
+              <span class="label label-warning pull-right">soon</span>
+            </span>
+          </a>
+
+          <ul class="treeview-menu">
+            <li>
+              <a href="<?php echo site_url('reports/student_reports'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Student Reports
+              </a>
+            </li>         
+            <li>
+              <a href="<?php echo site_url('enrollment/register_student'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                Grade Reports
+              </a>
+            </li>            
+          </ul>          
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-cog"></i>
+            <span>Settings</span>
+            <span class="pull-right-container">
+              <span class="label label-warning pull-right">soon</span>
+            </span>
+          </a>
+
+          <ul class="treeview-menu">
+            <li>
+              <a href="<?php echo site_url('enrollment/dashboard'); ?>">
+                <i class="fa fa-circle-o text-aqua"></i>
+                User Management
+              </a>
+            </li>                               
+          </ul>          
+        </li>
+       
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!--<div class="alert bg-navy alert-dismissible flat">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+      <h4><i class="icon fa fa-bullhorn"></i> Reminder!</h4>
+        Make sure to receive the necessary requirements for enrollment, otherwise the student won't be enrolled!
+     </div>-->
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Search for Registered Students
+        <small>Pick one to enroll</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Enrollment</a></li>
+        <li class="active">Enroll Student</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Enrolled Students</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="enrolledStudentstable" class="table table-bordered table-hover">
+                <thead>
+                <tr>                  
+                  <th>ID</th>
+                  <th>LRN</th>
+                  <th>Name</th>
+                  <th>Date Enrolled</th>
+                </tr>
+                </thead>
+                <tfoot>
+                <tr>
+                  <th>ID</th>
+                  <th>LRN</th>
+                  <th>Name</th>
+                  <th>Date Enrolled</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  {footer}
+
+  
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+
+
+<!-- jQuery 3 -->
+<script src="<?php echo base_url(); ?>bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url(); ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="<?php echo base_url(); ?>bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url(); ?>bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url(); ?>bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url(); ?>dist/js/adminlte.min.js"></script>
+<!-- Select2 -->
+<script src="<?php echo base_url(); ?>bower_components/select2/dist/js/select2.full.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url(); ?>dist/js/demo.js"></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url(); ?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+
+<script>
+  var getRecordsUrl = '<?php echo base_url("enrollment/enroll_student/populateTable2"); ?>';
+
+  $('#enrolledStudentstable').DataTable().destroy();
+
+  $('#enrolledStudentstable').DataTable({
+    "columns": [
+        { "width": "20%" },
+        { "width": "30%" },
+        { "width": "25%" },
+        { "width": "25%" }
+        ],
+        "order": [] ,
+        "ajax": getRecordsUrl
+  });
+</script>
+
+
+</body>
+</html>
