680cbf11debf9f7e85db6ff7eb0fedfea40c8efc
jasveranlouise <jassysalva@gmail.com>
2018-01-18 01:17:10 +0800

    advisory ui not done yet

---
+++ application/controllers/advisory/advisory_class.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class advisory_class extends CI_Controller {
+
+	public function __construct()
+	{
+	    parent::__construct();
+	    $this->sms_session->checkSession();
+	    // $this->load->model('dashboard_model');
+	}
+
+	public function index()
+	{	
+		$data = $this->parse->parsed();
+
+		// $data['teachersCount'] = $this->global_model->count('teachers');
+		// $data['roomsCount'] = $this->global_model->count('rooms');
+		// $data['subjectsCount'] = $this->global_model->count('subjects');
+		// $data['enrolledStudentsCount'] = $this->global_model->count('enrolled_students');
+		// $data['students_enrolled'] = $this->dashboard_model->getStudents();
+		// foreach ($data['students_enrolled'] as $key => $val) {
+		// 	$code = $this->dashboard_model->getStrand($val->section_id);
+		// 	$data['students_enrolled'][$key]->strand_name = $code;
+		// };
+
+		$data['active'] = 'advisory/advisory_class';
+		$data['template'] = $this->load->view('template/sidenav', $data, TRUE);
+
+    	$this->parser->parse('advisory/advisory_class', $data);
+	}
+
+}
---
+++ application/views/advisory/advisory_class.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>School Management | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/skins/_all-skins.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <!-- Loading -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/loading.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/  /3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/googlefont.css">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+<div hidden class="loading">Loading&#8230;</div>
+  
+  <?=$template?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- <div class="alert alert-success alert-dismissible flat">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+      <h4><i class="icon fa fa-bullhorn"></i> Day 1 of Enrollment is successful!</h4>
+        Congratulations! Job well done! Please do the same on Day 2 of Enrollment!
+     </div> -->
+      
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Advisory Class
+        <!-- <small>Student Grades</small> -->
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="enrollment/dashboard"><i class="fa fa-user"></i> Advisory</a></li>
+        <li class="active">Advisory Class</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <h4 class="box-title">Select Criteria</h4>
+              <!-- <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                  <i class="fa fa-minus"></i>
+                </button>
+              </div> -->
+            </div>
+            <div class="box-body">
+              <div class="col-xs-12 col-lg-4">
+                  <div class="form-group">
+                    <label>School Year</label>
+                      <select class="subject-input form-control select2" style="width: 100%;">
+                        <option>2017-2018</option>
+                        <option>2018-2019</option>
+                      </select>
+                  </div> 
+              </div>
+              <div class="col-xs-12 col-lg-4">
+                  <div class="form-group">
+                    <label>Semester</label>
+                      <select class="subject-input form-control select2" style="width: 100%;">
+                        <option>1st Semester</option>
+                        <option>2nd Semester</option>
+                      </select>
+                  </div> 
+              </div>
+              <div class="col-xs-12 col-lg-4">
+                  <div class="form-group">
+                    <label>Section</label>
+                      <input type="text" class="form-control" name="" disabled="">
+                  </div> 
+              </div>
+            </div>
+          </div>
+          <!-- /.box -->
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title"></h3>
+              <!-- <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                  <i class="fa fa-minus"></i>
+                </button>
+              </div> -->
+            </div>
+            <div class="box-body">
+              <br>
+              <table id = "studentList1" class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>LRN</th>
+                    <th>Full Name</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>123112312</td>
+                    <td>Escaro, Adrielle Kristine Nicolette M.</td>
+                    <td>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-check text-success'></span></button>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-edit text-info'></span></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>144231231</td>
+                    <td>Polistico, Hakeem A.</td>
+                    <td>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-check text-success'></span></button>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-edit text-info'></span></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>144231231</td>
+                    <td>Polistico, Hakeem A.</td>
+                    <td>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-check text-success'></span></button>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-edit text-info'></span></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>144231231</td>
+                    <td>Polistico, Hakeem A.</td>
+                    <td>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-check text-success'></span></button>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-edit text-info'></span></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>144231231</td>
+                    <td>Polistico, Hakeem A.</td>
+                    <td>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-check text-success'></span></button>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-edit text-info'></span></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>144231231</td>
+                    <td>Polistico, Hakeem A.</td>
+                    <td>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-check text-success'></span></button>
+                      <button class='btn btn-default btn-xs'><span class='fa fa-fw fa-edit text-info'></span></button>
+                    </td>
+                  </tr>
+                </tbody>
+                <tfoot>
+                  
+                </tfoot>
+              </table><hr>
+              
+              <button class='btn btn-primary pull-right' data-toggle="modal" data-target="#modal-default">Submit<!-- <span class='fa fa-fw fa-check'></span> --></button>
+            </div>
+          </div>
+          <!-- /.box -->
+        </div>
+      </div>
+      
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  {footer}
+</div>
+<!-- ./wrapper -->
+
+<script src="<?php echo base_url(); ?>bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url(); ?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="<?php echo base_url(); ?>bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url(); ?>bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<!-- ChartJS -->
+<script src="<?php echo base_url(); ?>bower_components/Chart.js/Chart.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url(); ?>bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url(); ?>dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url(); ?>dist/js/demo.js"></script>
+<!-- page script -->
+<script src="<?php echo base_url(); ?>dist/js/enrollment/dashboard.js"></script>
+
+</body>
+</html>
