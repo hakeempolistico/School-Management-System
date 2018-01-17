699b9e202a7a31958f627caf8b7db4dd8bed073d
jasveranlouise <jassysalva@gmail.com>
2018-01-18 03:34:54 +0800

    added sidenav

--- application/views/template/sidenav.php
+++ application/views/template/sidenav.php
@@ -196,7 +196,17 @@
               </a>
             </li>
           </ul>          
-        </li>      
+        </li>
+
+        <li class="<?php echo (isset($active) && $active =='advisory/advisory_class' ? 'active' : null); ?>">
+          <a href="<?php echo site_url('advisory/advisory_class'); ?>">
+            <i class="fa fa-file"></i>
+            <span>Advisory Class</span>
+          </a>
+        </li>
+
+        <li class="<?php echo (isset($active) && $active =='grades/grade_student' ? 'active' : null); ?>">
+          <a href="<?php echo site_url('grades/grade_student'); ?>">
         <li class="treeview <?php echo (isset($active) && substr($active,0,6) =='grades' ? 'active' : null); ?>">
           <a href="">
             <i class="fa fa-file"></i>
